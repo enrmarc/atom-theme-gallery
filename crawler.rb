7d0e8ca8f2c675cc2062aa9d15c3142c0e234fc8
enrmarc <enrmarc@gmail.com>
2015-05-02 19:40:13 +0200

    It's working

--- crawler.rb
+++ crawler.rb
@@ -1,8 +1,28 @@
 require 'bundler/setup'
+
 require 'nokogiri'
 require 'open-uri'
+require 'json'
 
-$BASE_URL = 'https://atom.io/themes/list?direction=desc'
+ATOM_URL = 'https://atom.io'
 
-page = Nokogiri::HTML open $BASE_URL
-puts page.class
+themes = []
+
+doc = Nokogiri::HTML open "#{ATOM_URL}/themes/list?direction=desc"
+doc.css('.package-list .grid-cell').each do |node|
+   card = node.css('.package-card')
+
+   name = card.css('.card-name a')
+   desc = card.css('.card-description')
+   link = name[0]['href']
+   img = (Nokogiri::HTML open "#{ATOM_URL}#{link}").css('.markdown-body img').first
+
+   themes.push({
+      :name => name.text,
+      :desc => desc.text,
+      :link => link,
+      :img  => img['src']
+   })
+end
+
+puts themes.to_json
