bb744265e27694a419341ade8fa5c4f58e1f5972
enrmarc <enrmarc@gmail.com>
2015-05-03 14:18:21 +0200

    Iterate over all the pages

--- crawler.rb
+++ crawler.rb
@@ -5,24 +5,29 @@ require 'open-uri'
 require 'json'
 
 ATOM_URL = 'https://atom.io'
+PAGES    = 20
+themes   = []
 
-themes = []
+1.upto(PAGES) do |page|
+   doc = Nokogiri::HTML open "#{ATOM_URL}/themes/list?direction=desc&page=#{page}"
+   i = 0
+   doc.css('.package-list .grid-cell').each do |node|
+      STDERR.puts "page: #{page}, theme #{i}"
+      card = node.css('.package-card')
 
-doc = Nokogiri::HTML open "#{ATOM_URL}/themes/list?direction=desc"
-doc.css('.package-list .grid-cell').each do |node|
-   card = node.css('.package-card')
+      name = card.css('.card-name a')
+      desc = card.css('.card-description')
+      link = name[0]['href']
+      img = (Nokogiri::HTML open "#{ATOM_URL}#{link}").css('.markdown-body img').first
 
-   name = card.css('.card-name a')
-   desc = card.css('.card-description')
-   link = name[0]['href']
-   img = (Nokogiri::HTML open "#{ATOM_URL}#{link}").css('.markdown-body img').first
-
-   themes.push({
-      :name => name.text,
-      :desc => desc.text,
-      :link => link,
-      :img  => img['src']
-   })
+      themes.push({
+         :name => name.text,
+         :desc => desc.text,
+         :link => link,
+         :img  => img && img['src']
+      })
+      i += 1
+   end
 end
 
 puts themes.to_json
